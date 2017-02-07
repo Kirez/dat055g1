c1dc7a9fe80920f01de8f342eda8a200b0de6677
KarlAngermark <kalle@angermark.se>
2017-02-07 16:05:25 +0100

    skdji

--- src/client/PlayerRenderer.java
+++ src/client/PlayerRenderer.java
@@ -20,10 +20,12 @@ public class PlayerRenderer implements GameRenderer {
     GraphicsContext gc = canvas.getGraphicsContext2D();
     gc.setFill(Color.RED);
     gc.fillOval(player.getPosition().getX(), player.getPosition().getY(), 16, 16);
-    gc.fillRect(player.getHurtboxes(0).getHeadBoxX(), player.getHurtboxes(0).getHeadBoxY(), player.getHurtboxes(0).getBoxWidth(), player.getHurtboxes(0).getBoxHeight());
-    gc.fillRect(player.getHurtboxes(1).getBodyBoxX(), player.getHurtboxes(1).getBodyBoxY(), player.getHurtboxes(1).getBoxWidth(), player.getHurtboxes(1).getBoxHeight());
+    gc.fillRect(player.getHurtboxes(0).getHeadBoxX(), player.getHurtboxes(0).getHeadBoxY(),
+        player.getHurtboxes(0).getBoxWidth(), player.getHurtboxes(0).getBoxHeight());
+    gc.fillRect(player.getHurtboxes(1).getBodyBoxX(), player.getHurtboxes(1).getBodyBoxY(),
+        player.getHurtboxes(1).getBoxWidth(), player.getHurtboxes(1).getBoxHeight());
 
-    if (PlayerController.hit == true) {
+ /*   if (PlayerController.hit == true) {
       gc.setFill(Color.YELLOW);
       gc.fillRect(player.getHitboxes(0).getPunchX(), player.getHitboxes(0).getPunchY(),
           player.getHitboxes(0).getBoxWidth(), player.getHitboxes(0).getBoxHeight());
@@ -31,6 +33,7 @@ public class PlayerRenderer implements GameRenderer {
       gc.setFill(Color.WHITE);
       gc.fillRect(player.getHitboxes(0).getPunchX(), player.getHitboxes(0).getPunchY(),
           player.getHitboxes(0).getBoxWidth(), player.getHitboxes(0).getBoxHeight());
-    }
+    }*/
   }
+}
 
