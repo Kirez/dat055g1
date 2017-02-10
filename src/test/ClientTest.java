6e0591a42b48e110c4c07de579451db74fa35531
Linus Berglund <MrZombax@gmail.com>
2017-02-10 18:04:51 +0100

    Create test skeletons

---
+++ src/test/ClientTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ClientTest {
+
+  @BeforeEach
+  void setUp() {
+
+  }
+
+  @AfterEach
+  void tearDown() {
+
+  }
+
+  @Test
+  void alwaysTrue() {
+    assertTrue(true);
+  }
+
+}
