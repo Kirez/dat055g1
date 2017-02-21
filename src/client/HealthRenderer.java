ed175eec42d7d6de8cabc48c60dbeff8703313f9
Timmy Truong <Timmy1228@hotmail.com>
2017-02-21 14:16:12 +0100

    Created (incomplete) HealthRenderer class

---
+++ src/client/HealthRenderer.java
@@ -0,0 +1,22 @@
+package client;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+/**
+ * Created by timmy on 2017-02-14.
+ */
+/*
+private GamePlayer player;
+
+public class HealthRenderer implements GameRenderer{
+
+    @Override
+    public void render(Canvas canvas) {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setStroke(Color.RED);
+
+    }
+}
+*/
