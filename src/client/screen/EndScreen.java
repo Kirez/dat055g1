4a3f64259602822f9c2dd21fe95cfd99e220eb6c
Linus Berglund <MrZombax@gmail.com>
2017-02-28 16:34:14 +0100

    Create skeleton EndScreen

---
+++ src/client/screen/EndScreen.java
@@ -0,0 +1,16 @@
+package client.screen;
+
+import javafx.stage.Stage;
+
+public class EndScreen implements Screen{
+
+  @Override
+  public void enter(Stage stage) {
+
+  }
+
+  @Override
+  public void exit() {
+
+  }
+}
