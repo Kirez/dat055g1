5a4418be6852da66b63b902385bc38690f95dfa8
Linus Berglund <MrZombax@gmail.com>
2017-02-28 13:17:45 +0100

    Optimize imports in FileHandler.java

--- src/client/FileHandler.java
+++ src/client/FileHandler.java
@@ -1,12 +1,12 @@
 package client;
 
-import javax.xml.parsers.SAXParserFactory;
-import javax.xml.parsers.*;
-import org.xml.sax.*;
-import org.xml.sax.helpers.*;
 import common.GamePlayer;
-    import java.util.*;
-    import java.io.*;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
 public class FileHandler {
   double bHeight;
   double bWidth;
